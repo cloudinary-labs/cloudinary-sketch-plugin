d4fac05bace26fbbab416a784c5cbb4bd451da5c
Itay Taragano <itay.taragano@cloudinary.com>
2019-05-26 19:30:46 +0300

    support uploading from artboard

--- src/upload.js
+++ src/upload.js
@@ -5,29 +5,38 @@ const { UI, Settings } = sketch
 
 export function upload() {
     const { cloud_name } = getStoredCredentials();
+    var artboards = sketch.getSelectedDocument().selectedLayers.layers;
+    console.log(artboards);
+    artboards.forEach(function(ab){
+    	if (ab && ab.selected) console.log(ab.name);
+    	const options = { formats: 'png', output: false};
+			const buffer = sketch.export(ab, options);
+			// const file = new File(buffer, "file.png");
+			console.log(typeof buffer)
+    	var data = new FormData();
+    	//{
+        data.append("upload_preset", "preset",);
+        data.append("file", "data:image/png;base64," + buffer.toString('base64'));
+        // data.append("file", new Blob([buffer]));
+        data.append("folder", "myfolder"); //folder input from user
+        data.append("public_id", ab.name); //artboard name - auto generate
+	    //};
 
+	    fetch(`https://api.cloudinary.com/v1_1/${cloud_name}/upload`, {
+	    	method: 'POST',
+	    	headers: {
+	    		'Content-Type': 'application/json'
+	    	},
+	    	body: data
+	    }).then(function(response){return response.json();})
+	    .then(function(data) {
+	        console.log(data);
+		  });
+    })
     if (!cloud_name) {
         UI.alert('⚠️ You need to configure Cloudinary account');
         return -1;
     }
-
-    var data = {
-        "upload_preset": "preset",
-        "file": "https://res.cloudinary.com/demo/w_50/sample.jpg", //
-        "folder": "myfolder", //folder input from user
-        "public_id": "artboard_name" //artboard name - auto generate
-    };
-
-    fetch(`https://api.cloudinary.com/v1_1/${cloud_name}/upload`, {
-    	method: 'POST',
-    	headers: {
-    		'Content-Type': 'application/json'
-    	},
-    	body: JSON.stringify(data)
-    }).then(function(response){return response.json();})
-    .then(function(data) {
-        console.log(data);
-	  });
 }
 
 export default upload;
