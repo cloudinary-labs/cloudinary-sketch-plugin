639d4e49e500e952c789e94f37a8e7a9a6bcc569
Itay Taragano <itay.taragano@cloudinary.com>
2019-05-26 17:01:14 +0300

    initial upload.js commit

--- src/upload.js
+++ src/upload.js
@@ -1,1 +1,26 @@
-export function upload() {}
+var cloud_name = NSUserDefaults.standardUserDefaults().objectForKey('cloudname');
+var api_key = NSUserDefaults.standardUserDefaults().objectForKey('apikey');
+var api_secret = NSUserDefaults.standardUserDefaults().objectForKey('secretkey');
+
+var data = {
+    "upload_preset": "preset",
+    "file": "https://res.cloudinary.com/demo/w_50/sample.jpg",
+    "folder": "myfolder",
+    "public_id": "artboard_name"
+};
+
+export function search(cloudName, apiKey, secretKey) {
+    if (!cloudName || !apiKey || !secretKey) return -1;
+    fetch(`https://api.cloudinary.com/v1_1/${cloud_name}/upload`, {
+    	method: 'POST',
+    	headers: {
+    		'Content-Type': 'application/json'
+    	},
+    	body: JSON.stringify(data)
+    }).then(function(response){return response.json();})
+    .then(function(data) {
+        console.log(data);
+	  });
+}
+
+search(cloud_name,api_key,api_secret);
