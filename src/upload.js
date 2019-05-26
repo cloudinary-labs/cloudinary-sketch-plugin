efe9333c293c5c0f340dae3177131842f3a03e9c
Maya Shavin <maya@cloudinary.com>
2019-05-26 19:51:59 +0300

    fix text

--- src/upload.js
+++ src/upload.js
@@ -22,29 +22,35 @@ export function upload() {
             const terms = folder.trim();
 
             artboards.forEach(function(ab){
-                if (ab && ab.selected) console.log(ab.name);
+                if (ab && ab.selected) {
+                    const options = { formats: 'png', output: false};
+                    const buffer = sketch.export(ab, options);
 
-                const options = { formats: 'png', output: false};
-                const buffer = sketch.export(ab, options);
-                var data = new FormData();
-                data.append("upload_preset", "preset",); //user generated
-                data.append("file", "data:image/png;base64," + buffer.toString('base64'));
-                data.append("folder", terms); //folder input from user
-                data.append("public_id", ab.name); //artboard name - auto generate
-        
-                UI.message('⏰ Uploading, please wait ...');
-                fetch(`https://api.cloudinary.com/v1_1/${cloud_name}/upload`, {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: data
-                })
-                .then(function(response){return response.json();})
-                .then(function(data) {
-                    console.log(data);
-                    UI.message('🚀 Uploaded successfully!!!');
-                  });
+                    var data = new FormData();
+                    data.append("upload_preset", "preset",); //user generated
+                    data.append("file", "data:image/png;base64," + buffer.toString('base64'));
+                    data.append("folder", terms); //folder input from user
+                    data.append("public_id", ab.name); //artboard name - auto generate
+            
+                    UI.message('⏰ Uploading, please wait ...');
+                    fetch(`https://api.cloudinary.com/v1_1/${cloud_name}/upload`, {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: data
+                    })
+                    .then(function(response){return response.json();})
+                    .then(function(data) {
+                        console.log(data);
+
+                        UI.message('🚀 Uploaded successfully!!!');
+                    });
+                }
+                else {
+                    UI.message('To upload, select an artboard.');
+                }
+                
             })
         }
     )
